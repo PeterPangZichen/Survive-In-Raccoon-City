af926a8884f9f248128af778a11812bc27cbf63d
xalphinions@outlook.com <xalphinions@outlook.com>
2022-07-15 20:56:25 +0800

    ADD: Add the weapon assets, with player using a deagle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,12 +18,17 @@ public class PlayerController : MonoBehaviour
     public bool GetfaceRightState(){
         return faceRightState;
     }
+
+    private GameObject WeaponObject;
+    private GameObject FirePointObject;
     
     // Start is called before the first frame update
     void Start()
     {
         playerSprite = GetComponent<SpriteRenderer>();
-        playerBody = GetComponent<Rigidbody2D>();        
+        playerBody = GetComponent<Rigidbody2D>();   
+        WeaponObject = gameObject.transform.GetChild(1).gameObject;     
+        FirePointObject = gameObject.transform.GetChild(1).gameObject;    
     }
 
     // Update is called once per frame
@@ -35,12 +40,32 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown("a") && faceRightState){
             faceRightState = false;
             playerSprite.flipX = true;
+            // flip the weapon also!
+            Vector3 scale = WeaponObject.transform.localScale;
+            scale.x *= -1;
+            WeaponObject.transform.localScale = scale;
+            Vector3 scale2 = FirePointObject.transform.localScale;
+            scale2.x *= -1;
+            FirePointObject.transform.localScale = scale;
+            WeaponObject.transform.localPosition = new Vector3(-0.68f,0.6f,0f);
+            FirePointObject.transform.localPosition = new Vector3(-1.05f,0.73f,0f);
         }
 
         if (Input.GetKeyDown("d") && !faceRightState){
             faceRightState = true;
             playerSprite.flipX = false;
+            // flip the weapon
+            Vector3 scale = WeaponObject.transform.localScale;
+            scale.x *= -1;
+            WeaponObject.transform.localScale = scale;
+            Vector3 scale2 = FirePointObject.transform.localScale;
+            scale2.x *= -1;
+            FirePointObject.transform.localScale = scale;
+            WeaponObject.transform.localPosition = new Vector3(0.68f,0.6f,0f);
+            FirePointObject.transform.localPosition = new Vector3(1.05f,0.73f,0f);
         }
+
+        
     }
 
     private void FixedUpdate()
