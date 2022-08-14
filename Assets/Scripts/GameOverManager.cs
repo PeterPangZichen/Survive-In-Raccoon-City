d08f8e0d4aa5360496886d1ae335a4ebbc242bc7
PeterPangZichen <72613056+PeterPangZichen@users.noreply.github.com>
2022-08-15 07:40:30 +0800

    Minor changes

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
+    public MenuData menudata;
     // Start is called before the first frame update
     void Start()
     {
